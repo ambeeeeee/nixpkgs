48c31fa40b6afde8724fd399bf08a52db9dd2c96
Amber Kowalski <amberkowalski03@gmail.com>
2023-08-01 19:04:43 -0500

    Fuck hashesh

--- pkgs/development/libraries/aasdk/default.nix
+++ pkgs/development/libraries/aasdk/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
     src = fetchgit {
         url = "https://github.com/openDsh/aasdk.git";
         rev = "1bc0fe69d5f5f505c978a0c6e32c860e820fa8f6";
-        sha256 = "sha256-bIERpPsIeBPNxUji7c7mV+LJmP54Y3tD2NAqd158lTY=";
+        sha256 = "sha256-Gqd+IHn3G3yU1/SSrp8B+isn1mhsGj2w41oMmSgkpQY=";
     };
 
     patches = [
